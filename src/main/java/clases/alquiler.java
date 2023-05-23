f9e9eeb60572e2e27d9fa845d752b769ab1edc22
xjosejurado21 <josejuradocenec@gmail.com>
2023-05-23 18:25:14 +0200

    clase alquiler
    implementando variables, constructores,getter y setters etc...

--- src/main/java/clases/alquiler.java
+++ src/main/java/clases/alquiler.java
@@ -1,5 +1,42 @@
 package clases;
 
-public class alquiler {
+import java.util.List;
 
+public class alquiler {
+    private String tipo;
+    private float precio;
+    private List<servicio> servicios; 
+
+    public alquiler(String tipo, float precio, List<servicio> servicios) {
+        this.tipo = tipo;
+        this.precio = precio;
+        this.servicios = servicios;
+    }
+
+    // Getters and setters
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public List<servicio> getServicios() {
+        return servicios;
+    }
+
+    public void setServicios(List<servicio> servicios) {
+        this.servicios = servicios;
+    }
 }
+

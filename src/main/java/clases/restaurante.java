645260c3eb57e2f68d5798943f175eb042061b92
xjosejurado21 <josejuradocenec@gmail.com>
2023-05-23 19:32:52 +0200

    Clase restaurante
    contructores,getters,setters...

--- src/main/java/clases/restaurante.java
+++ src/main/java/clases/restaurante.java
@@ -1,5 +1,31 @@
 package clases;
 
-public class restaurante  {
-	
-	}
+public class restaurante extends elementoConNombre {
+    private String tipoComida;
+    private int mesas;
+
+    public restaurante(String nombre, String tipoComida, int mesas) {
+        super(nombre);
+        this.tipoComida = tipoComida;
+        this.mesas = mesas;
+    }
+
+    // Getters and setters
+
+    public String getTipoComida() {
+        return tipoComida;
+    }
+
+    public void setTipoComida(String tipoComida) {
+        this.tipoComida = tipoComida;
+    }
+
+    public int getMesas() {
+        return mesas;
+    }
+
+    public void setMesas(int mesas) {
+        this.mesas = mesas;
+    }
+}
+

1ab2a66d1d516699caf0a4ff881854924011b615
xjosejurado21 <josejuradocenec@gmail.com>
2023-05-23 19:45:38 +0200

    clase museo y otrasActiviades
    variables,constructores,getters,setters....

--- src/main/java/clases/museo.java
+++ src/main/java/clases/museo.java
@@ -1,4 +1,29 @@
 package clases;
-public class museo  {
-	
-	}
+public class museo extends elementoConNombre {
+    private String tema;
+    private int numSalas;
+    public museo(String nombre, String tema, int numSalas) {
+        super(nombre);
+        this.tema = tema;
+        this.numSalas = numSalas;
+    }
+
+    // Getters and setters
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public int getNumSalas() {
+        return numSalas;
+    }
+
+    public void setNumSalas(int numSalas) {
+        this.numSalas = numSalas;
+    }
+}
+
--- src/main/java/clases/otrasActividades.java
+++ src/main/java/clases/otrasActividades.java
@@ -1,5 +1,43 @@
 package clases;
 
-public class otrasActividades  {
-	
-	}
+import java.util.Date;
+
+public class otrasActividades extends elementoConNombre {
+    private String descripcion;
+    private Date fechaInicio;
+    private Date fechaSalida;
+
+    public otrasActividades(String nombre, String descripcion, Date fechaInicio, Date fechaSalida) {
+        super(nombre);
+        this.descripcion = descripcion;
+        this.fechaInicio = fechaInicio;
+        this.fechaSalida = fechaSalida;
+    }
+
+    // Getters and setters
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+}
+

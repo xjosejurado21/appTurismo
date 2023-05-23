ceb64dd4efd3cdde868dc57889f75bf25fe87e77
xjosejurado21 <josejuradocenec@gmail.com>
2023-05-23 19:29:14 +0200

    clase hotel
    implementando constructores,getters,setters, etc...

--- src/main/java/clases/hotel.java
+++ src/main/java/clases/hotel.java
@@ -1,5 +1,21 @@
 package clases;
-public class hotel {
-	
+public class hotel extends elementoConNombre {
+    private int estrellas;
+
+    public hotel(String nombre, int estrellas) {
+        super(nombre);
+        this.estrellas = estrellas;
+    }
+
+    // Getters and setters
+
+    public int getEstrellas() {
+        return estrellas;
+    }
+
+    public void setEstrellas(int estrellas) {
+        this.estrellas = estrellas;
+    }
 }
+
 	

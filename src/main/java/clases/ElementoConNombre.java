a7b80f04bb0a9f0aa1703a446b89706d7355de5d
xjosejurado21 <josejuradocenec@gmail.com>
2023-05-23 18:04:51 +0200

    Rellenamos las clases elementoConNombre y usuario
    creando las variables correspondientes. constructores, metodos,getters y settters ...

--- src/main/java/clases/ElementoConNombre.java
+++ src/main/java/clases/ElementoConNombre.java
@@ -1,5 +1,22 @@
 package clases;
 
-public class ElementoConNombre {
-	
-	}
+public class elementoConNombre {
+    // Atributos
+    private String nombre;
+
+    // Constructor
+    public elementoConNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    //ToString
+}
+
--- src/main/java/clases/usuario.java
+++ src/main/java/clases/usuario.java
@@ -1,6 +1,86 @@
 package clases;
 
 //Clase usuario
-public class usuario {
+public class usuario extends elementoConNombre{
+    // Atributos
+    private int id;
+    private String nombre;
+    private String email;
+    private boolean isAdmin;
+    private String contrasena;
 
+    // Constructor
+    public usuario(String nombre, int id, String nombreUsuario, String email, boolean isAdmin, String contrasena) {
+  		super(nombre);
+  		this.id = id;
+  		nombre = nombreUsuario;
+  		this.email = email;
+  		this.isAdmin = isAdmin;
+  		this.contrasena = contrasena;
+  	}
+    // Metodos
+    public void crear_usuario() {
+        // Implementar la lógica para crear un usuario
+    }
+
+	public void eliminar_usuario() {
+        // Implementar la lógica para eliminar un usuario
+    }
+
+    public void crear_contrasena() {
+        // Implementar la lógica para crear una contraseña
+    }
+
+    public void modificar_contrasena() {
+        // Implementar la lógica para modificar una contraseña
+    }
+
+    public void modificar_nombre() {
+        // Implementar la lógica para modificar el nombre
+    }
+
+    public void crear_valoracion() {
+        // Implementar la lógica para crear una valoración
+    }
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
 }

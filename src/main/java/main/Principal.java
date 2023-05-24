45b13b3ca20da38ac3d560da470f5c94555da692
xjosejurado21 <josejuradocenec@gmail.com>
2023-05-24 12:48:11 +0200

    Interfaz Grafica
    Retocando el menu de inicio y registrar

--- src/main/java/main/Principal.java
+++ src/main/java/main/Principal.java
@@ -19,7 +19,6 @@ import java.util.Map;
 import javax.swing.*;
 import java.awt.*;
 
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -27,8 +26,27 @@ import javax.swing.*;
 import java.awt.*;
 import java.sql.*;
 
-public class Principal {
+import java.sql.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+import java.sql.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import java.sql.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import java.sql.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Principal {
     private static Connection connection;
 
     public static void main(String[] args) {
@@ -43,11 +61,6 @@ public class Principal {
             // Crea el JPanel
             JPanel panel = new JPanel();
             panel.setLayout(new BorderLayout());
-
-            // Imagen
-            ImageIcon imageIcon = new ImageIcon("path-to-your-image"); // Reemplaza con el path de tu imagen
-            JLabel imageLabel = new JLabel(imageIcon);
-            panel.add(imageLabel, BorderLayout.CENTER);
 
             // Campos de texto y botones
             JTextField userField = new JTextField(20);
@@ -99,29 +112,65 @@ public class Principal {
 
             // Acción para el botón de registro
             registerButton.addActionListener(e -> {
-                try {
-                    PreparedStatement ps = connection.prepareStatement("INSERT INTO Usuario (Nombre, Contraseña) VALUES (?, ?)");
-                    ps.setString(1, userField.getText());
-                    ps.setString(2, new String(passwordField.getPassword()));
-                    ps.executeUpdate();
+                // Crea una nueva ventana de registro
+                JFrame registerFrame = new JFrame("Registro");
+                registerFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-                    JOptionPane.showMessageDialog(loginFrame, "Registro exitoso. Ahora puedes iniciar sesión.");
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
+                // Crea un panel con los campos para el registro
+                JPanel registerPanel = new JPanel();
+                JTextField registerUserField = new JTextField(20);
+                JPasswordField registerPasswordField = new JPasswordField(20);
+                JTextField registerEmailField = new JTextField(20);  // Campo para el correo electrónico
+                JButton confirmRegisterButton = new JButton("Confirmar Registro");
+                registerPanel.add(new JLabel("Usuario:"));
+                registerPanel.add(registerUserField);
+                registerPanel.add(new JLabel("Contraseña:"));
+                registerPanel.add(registerPasswordField);
+                registerPanel.add(new JLabel("Email:"));  // Etiqueta para el campo de correo electrónico
+                registerPanel.add(registerEmailField);  // Añade el campo de correo electrónico al panel
+                registerPanel.add(confirmRegisterButton);
+                registerFrame.add(registerPanel);
+                registerFrame.setSize(400, 200);
+                registerFrame.setLocationRelativeTo(null);
+                registerFrame.setVisible(true);
 
-                // Limpia los campos de texto
-                userField.setText("");
-                passwordField.setText("");
+                // Acción para el botón de confirmación de registro
+                confirmRegisterButton.addActionListener(ev -> {
+                    try {
+                        // Comprueba si el correo electrónico ya existe en la base de datos
+                        PreparedStatement checkEmail = connection.prepareStatement("SELECT * FROM Usuario WHERE Email = ?");
+                        checkEmail.setString(1, registerEmailField.getText());
+                        ResultSet rs = checkEmail.executeQuery();
+
+                        if (rs.next()) {
+                            // Si el correo electrónico ya existe, muestra un mensaje al usuario
+                            JOptionPane.showMessageDialog(registerFrame, "Este correo electrónico ya ha sido registrado. Por favor, ingresa un correo electrónico diferente.");
+                        } else {
+                            // Si el correo electrónico no existe, inserta el nuevo usuario en la base de datos
+                            PreparedStatement ps = connection.prepareStatement("INSERT INTO Usuario (Nombre, Contraseña, Email, isAdmin) VALUES (?, ?, ?, ?)");
+                            ps.setString(1, registerUserField.getText());
+                            ps.setString(2, new String(registerPasswordField.getPassword()));
+                            ps.setString(3, registerEmailField.getText());
+                            ps.setBoolean(4, false);  // asumimos que todos los nuevos usuarios no son administradores
+                            ps.executeUpdate();
+
+                            JOptionPane.showMessageDialog(registerFrame, "Registro exitoso. Ahora puedes iniciar sesión.");
+                            registerFrame.dispose();  // Cierra la ventana de registro
+                        }
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                    }
+
+                    // Limpia los campos de texto
+                    registerUserField.setText("");
+                    registerPasswordField.setText("");
+                    registerEmailField.setText("");
+                });
             });
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
     }
-    
-    
-    
-    
 }
 
 // Aquí necesitarás implementar la clase MainPage
